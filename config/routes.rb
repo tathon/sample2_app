159bd0e59ae5bc3fd85fccd0fef38646f9699401
Chu Tat Hon <chutathon@yahoo.com>
2011-01-11 01:22:03 -0800

    Done with static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Sample2App::Application.routes.draw do
   get "pages/home"
 
   get "pages/contact"
+  
+  get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
